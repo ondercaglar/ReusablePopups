fab17e347acd439ce5fdab50b9e29e81a3750ee7
ondercaglar@gmail.com <8ggjk5435klg>
2018-12-20 17:12:38 +0300

    Initial Commit

---
+++ ReusablePopups/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ReusablePopups
+//
+//  Created by Boss on 20.12.2018.
+//  Copyright © 2018 Boss. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ReusablePopups/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ReusablePopups
+//
+//  Created by Boss on 20.12.2018.
+//  Copyright © 2018 Boss. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
